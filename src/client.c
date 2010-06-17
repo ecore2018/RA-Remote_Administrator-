2f078e3139e3faa4dfe92f57f219d35d028be8b8
D4RKN355-H4CK <federicogerardi94@gmail.com>
2010-06-17 18:27:48 +0200

    BUGS

--- src/client.c
+++ src/client.c
@@ -25,7 +25,6 @@
 
 #define PORT 6667
 #define MAXLINE 1000
-#define IP "127.0.0.1"
 
 int main( int argc, char *argv[] ){
 
@@ -41,7 +40,7 @@ int main( int argc, char *argv[] ){
    if( ( TCP = socket( AF_INET, SOCK_STREAM, 0 ) ) < 0 )
       die();
    
-   sock_init( &server, inet_addr( IP ), PORT );
+   sock_init( &server, inet_addr( argv[ 1 ] ), PORT );
     
    if( connect( TCP, ( struct sockaddr * ) &server, sizeof( server ) ) < 0 )
       die();
